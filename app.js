35349bd5c332561811832a20e4def8b77864e440
Dale Syth <dalesyth@gmail.com>
2023-07-26 22:18:15 -0500

    setting up server

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+/* eslint-disable */
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
 // Setup your Middleware and API Router here
+
+const {
+  USER: user,
+  HOST: host,
+  DATABASE: database,
+  PASSWORD: password,
+  PORT: port,
+  SERVER_PORT,
+} = process.env;
+
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const apiRouter = require("./api");
+app.use("/api", apiRouter);
+
+const { client } = require("./db/client");
+
+app.use((error, req, res, next) => {
+  res.status(500);
+  res.send({
+    name: error.name,
+    message: error.message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+  client.connect();
+});
 
 module.exports = app;
